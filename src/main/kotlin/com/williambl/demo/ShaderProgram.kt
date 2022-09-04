c7c14f97c7c76f69c675870788ed4b446ca0c5d0
williambl <william@williambl.com>
2022-09-04 21:44:51 +0100

    feat: add methods for setting shader uniform values

--- src/main/kotlin/com/williambl/demo/ShaderProgram.kt
+++ src/main/kotlin/com/williambl/demo/ShaderProgram.kt
@@ -1,6 +1,7 @@
 package com.williambl.demo
 
-import org.lwjgl.opengl.GL20.glUseProgram
+import org.lwjgl.opengl.GL20.*
+import org.lwjgl.opengl.GL40
 
 /**
  * A representation of an OpenGL Shader Program (i.e. a fragment and vertex shader pair).
@@ -8,7 +9,24 @@ import org.lwjgl.opengl.GL20.glUseProgram
  * Do not call the constructor, use [ShaderManager.getOrCreateShaderProgram] instead.
  */
 class ShaderProgram(val name: String, private val id: Int) {
+    private val uniforms: MutableMap<String, Int> = mutableMapOf()
+
     fun use() {
         glUseProgram(this.id)
+    }
+
+    fun setUniform(name: String, value: Mat4x4) {
+        val loc = this.uniforms.computeIfAbsent(name) { glGetUniformLocation(this.id, name) }
+        glUniformMatrix4fv(loc, false, value.forGl())
+    }
+
+    fun setUniform(name: String, value: Float) {
+        val loc = this.uniforms.computeIfAbsent(name) { glGetUniformLocation(this.id, name) }
+        glUniform1f(loc, value)
+    }
+
+    fun setUniform(name: String, value: Vec3) {
+        val loc = this.uniforms.computeIfAbsent(name) { glGetUniformLocation(this.id, name) }
+        glUniform3f(loc, value.x.toFloat(), value.y.toFloat(), value.z.toFloat())
     }
 }
