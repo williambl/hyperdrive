55c9cb09918ce4c167c6656881123fea1cdfb85a
williambl <william@williambl.com>
2022-09-08 14:23:19 +0100

    feat: separate texture interface/impl

---
+++ src/main/kotlin/com/williambl/demo/texture/TextureImpl.kt
@@ -0,0 +1,62 @@
+package com.williambl.demo.texture
+
+import com.williambl.demo.freeMemoryManaged
+import com.williambl.demo.toByteBuffer
+import com.williambl.demo.toMemoryManaged
+import org.lwjgl.opengl.GL45.*
+import org.lwjgl.stb.STBImage
+import java.io.IOException
+
+/**
+ * A representation of a texture.
+ *
+ * The texture is loaded from the jar resource at [location], from any format supported by [STBImage].
+ *
+ * Do not call the constructor, use [TextureManager.getOrCreateTexture] instead.
+ */
+class TextureImpl(val location: String) : Texture {
+    private val id: Int
+    val width: Int
+    val height: Int
+
+    init {
+        val imageData = try {
+            this::class.java.getResourceAsStream(this.location)?.toByteBuffer()?.toMemoryManaged() ?: throw IOException("Null Resource")
+        } catch (e: IOException) {
+            throw RuntimeException("\"${this.location}\" is not a valid texture", e)
+        }
+
+        val widthBuffer = IntArray(1)
+        val heightBuffer = IntArray(1)
+        val channelsBuffer = IntArray(1)
+
+        val texture = STBImage.stbi_load_from_memory(imageData, widthBuffer, heightBuffer, channelsBuffer, 4)
+
+        imageData.freeMemoryManaged()
+
+        if (texture == null) {
+            throw RuntimeException("\"${this.location}\" is not a valid texture: ${STBImage.stbi_failure_reason()}")
+        }
+
+        this.width = widthBuffer.first()
+        this.height = heightBuffer.first()
+
+        this.id = glGenTextures()
+
+        glBindTexture(GL_TEXTURE_2D, this.id)
+
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this.width, this.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture)
+    }
+
+    override fun bind() {
+        if (this.id == -1) {
+            println("\"${this.location}\" is not a valid texture")
+            return
+        }
+        glBindTexture(GL_TEXTURE_2D, this.id)
+    }
+}
--- src/main/kotlin/com/williambl/demo/texture/TextureManager.kt
+++ src/main/kotlin/com/williambl/demo/texture/TextureManager.kt
@@ -1,14 +1,14 @@
 package com.williambl.demo.texture
 
 object TextureManager {
-    private val textures: MutableMap<String, Texture> = mutableMapOf()
+    private val textures: MutableMap<String, TextureImpl> = mutableMapOf()
 
     /**
-     * Loads a [Texture] from the jar resource at [location].
+     * Loads a [TextureImpl] from the jar resource at [location].
      *
      * The texture file can be in any format supported by [org.lwjgl.stb.STBImage].
      */
     fun getOrCreateTexture(location: String): Texture {
-        return textures.getOrPut(location) { Texture(location) }
+        return this.textures.getOrPut(location) { TextureImpl(location) }
     }
 }
